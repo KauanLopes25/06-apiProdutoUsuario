fc472fc7c7d6495a6053d9354701d7de41e92707
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-09-03 09:56:38 -0300

    Adicionado estrutura de arquivos basica da API

--- server.js
+++ server.js
@@ -20,3 +20,25 @@
 * express() - FrameWork para facilitar a criação de API
 
 ********************************************************************************************/
+
+// Importar o módulo do Express
+const express = require('express')
+
+// Criar uma aplicação express
+const app = express()
+
+// Definir um Middleware para analisar Json no corpo das requisições
+app.use(express.json())
+
+// Definir a porta em que o servidor irá escutar
+const porta = 8000
+
+// Rota de teste API
+app.get('/', (req,res) => {
+    res.send("API de Produtos e Usuários está funcionando")
+})
+
+// Inicia o servidor
+app.listen(porta, () => {
+    console.log(`Servidor rodando em http://localhost:${porta}`)
+})
