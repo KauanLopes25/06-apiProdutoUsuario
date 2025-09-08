868102339d88ccfb2d917917bd6c1dd7a4b55abd
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-09-08 08:55:37 -0300

    Adicionado array da RAM para o DISCO. Falta testar

--- src/model/usuarioModel.js
+++ src/model/usuarioModel.js
@@ -14,11 +14,28 @@
 ********************************* COMANDOS UTILIZADOS ***************************************
 * find() - Retorna o primeiro valor encontrado com base no parametro fornecido
 * ... - Serve para adicionar elementos entre arrays.
+* fs - File System → módulo nativo do Node que permite ler e escrever arquivos.
+* path - Módulo do Node que ajuda a montar caminhos de arquivos de forma segura, 
+         independente do sistema operacional (Windows, Linux, Mac).
+* __dirname - Diretório atual do arquivo onde esse código está.
+* path.join(...) - Monta o caminho corretamente, sem precisar se preocupar com / ou \
+* JSON.stringify(array, null, 2) - Transforma o array em JSON formatado.
+                                    O null, 2 deixa o arquivo bonitinho, 
+                                    com indentação de 2 espaços.
+* fs.writeFileSync(...) - Sobrescreve o arquivo clientes.json com os dados novos.
+* 'utf-8' - Garante que o arquivo seja gravado em UTF-8.
+             UTF-8 é um padrão de codificação de texto que representa praticamente todos os 
+             caracteres usados no mundo.
 ******************************** BIBLIOTECAS UTILIZADAS *************************************
 
 ********************************************************************************************/
 
+// Constante para ler e escrever em arquivos
+const fs = require('fs')
+// Caminho dos dados de clientes 
 const users = require('../../data/clientes.json')
+// Caminho do dados de clientes
+const dataPath = path.join(__dirname, '../../data/clientes.json')
 
 // Função para retornar todos os usuáruos
 const findAll = () => {
@@ -42,7 +59,10 @@ const createNewUser = (newUser) => {
     // User[user.length - 1].id + 1                 Execução caso atenda a condição de comparação
     // :1                                           Execução caso não atenda, indicado pelo :
     const userWithId = {id: newId, ...newUser}
+    // Atualiza os dados na memória RAM
     users.push(userWithId)
+    // Atualiza os dados no disco
+    fs.writeFileSync(dataPath, JSON.stringify(users, null, 2), 'utf-8')
     return userWithId
 }
 
